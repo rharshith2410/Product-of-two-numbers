1bc3188c2f5dbcfac75ea6ec0743a6d2a2cc1032
rharshith2410 <68144219+rharshith2410@users.noreply.github.com>
2021-08-06 22:31:52 +0530

    Create Product.cpp

---
+++ Product.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    cout<<"Enter two numbers: ";
+    cin>> a >> b;
+    c = a*b;
+    cout<<"\n Product if two numbers is: "<<c;
+    return 0;
+
+}
